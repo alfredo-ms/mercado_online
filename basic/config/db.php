d269e33eb783f4fc9ed6f19c60d841b1ab7656ed
alfredo-ms <alfmagnanis@gmail.com>
2016-10-14 01:04:39 -0300

    Modificada a configuração da base de dados.

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=54.148.131.4;dbname=mercado_online',
+    'dsn' => 'mysql:host=localhost;dbname=mercado_online',
     'username' => 'root',
-    'password' => 'nekuro02',
+    'password' => '',
     'charset' => 'utf8',
 ];
